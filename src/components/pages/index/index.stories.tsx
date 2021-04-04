4b6fc6c414d040b00e4309ae3924c84c092f3155
Ren Adachi <49895682+moeyashi@users.noreply.github.com>
2021-04-04 20:41:21 +0900

    add screenshots

---
+++ src/components/pages/index/index.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { Meta } from '@storybook/react';
+
+import { Index } from '.';
+
+export default {
+  title: 'components/pages/index',
+  component: Index,
+} as Meta;
+
+export const Default = Index
